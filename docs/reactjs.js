f2dd33a418fee4b7770ca396fc60d3d236be8973
Adrien Baltardive <adrien@acte-solutions.com>
2021-02-11 13:27:32 +0700

    reactjs example

---
+++ docs/reactjs.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from "react";
+
+import tbClient from 'thingsboard-js-sdk';
+
+//tb server config
+const config = {
+  host: "localhost",
+  username: "username",
+  password: "secret"
+}
+
+//init client
+const client = new tbClient(config);
+
+//device
+const device = {
+  keys: ['temperature'],
+  limit: 10,
+  agg: 'AVG',
+  interval: 3600,
+  startTs: now-36000,
+  endTs: now,
+  entityId: "xxx-xxx-xxx"
+};
+
+
+//component
+const TbClientHome = (props) => {
+
+  const [ timeseries, setTimeseries ] = useState(null); //device timeseries
+  const [ connected, setConnected ] = useState(null); //client is connected
+
+
+  const connect = async () => {
+
+    const token = await client.connect(); // connect() returns token or null
+
+    if(token){
+      setConnected(true);
+      client.getTimeseries(device, setTimeseries);
+    } else {
+      alert('Login failed !!!');
+      setConnected(false);
+    }
+
+  }
+
+  useEffect(() => {
+    connect();
+  }, [])
+
+
+  if(connected && timeseries && timeseries.length > 0){
+
+    return(
+      <div>
+        <ul>
+        {timeseries.temperature && timeseries.temperature.map((item, key) =>
+          <li>{item.ts}: {item.value}</li>
+        )}
+        </ul>
+      </div>
+    );
+
+  } else {
+    return('Connecting...');
+  }
+
+}
+
+export default TbClientHome;
